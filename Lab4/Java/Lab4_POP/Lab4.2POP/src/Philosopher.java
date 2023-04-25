697cbbd26a7b31215f52dcb63fd2530dcb4aea04
BulatNick <80197859+ast0s@users.noreply.github.com>
2023-04-26 02:08:52 +0300

    Update Philosopher.java

--- Lab4/Java/Lab4_POP/Lab4.2POP/src/Philosopher.java
+++ Lab4/Java/Lab4_POP/Lab4.2POP/src/Philosopher.java
@@ -16,16 +16,23 @@ class Philosopher implements Runnable {
         try {
             for (int i = 0; i < 10; i++) {
                 System.out.println("Philosopher " + id + " thinking time " + i);
+
                 waiter.access.acquire();
+                System.out.println("Philosopher" + id + " asked the waiter to bring him cutlery");
+
                 leftFork.access.acquire();
                 System.out.println("Philosopher " + id + " took fork " + leftFork.id);
                 rightFork.access.acquire();
                 System.out.println("Philosopher " + id + " took fork " + rightFork.id);
+
                 System.out.println("Philosopher " + id + " eating time " + i);
+
                 System.out.println("Philosopher " + id + " put fork " + rightFork.id);
                 rightFork.access.release();
                 System.out.println("Philosopher " + id + " put fork " + leftFork.id);
                 leftFork.access.release();
+
+                System.out.println("Philosopher" + id + " was finished serving");
                 waiter.access.release();
             }
         } catch (InterruptedException e) {
